51e01a8497af9af7d438fabacca3be1fc8ffa517
Bartosz Pietrzyba <bartosz.pietrzyba@eficode.com>
2021-12-15 00:52:28 +0100

    Logging in page - functionally ok, looks will be changed

---
+++ lib/back4app/login.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final controllerUsername = TextEditingController();
+  final controllerPassword = TextEditingController();
+  bool isLoggedIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/logo/die.png',
+              scale: 0.5,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              controller: controllerUsername,
+              enabled: !isLoggedIn,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  labelText: 'Username'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextField(
+              controller: controllerPassword,
+              enabled: !isLoggedIn,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  labelText: 'Password'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 50,
+              child: TextButton(
+                child: const Text('SIGN IN'),
+                onPressed: isLoggedIn ? null : () => doUserLogin(),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 50,
+              child: TextButton(
+                child: const Text('SIGN UP'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/signUp');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void showSuccess(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Success!"),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error!"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void doUserLogin() async {
+    final username = controllerUsername.text.trim();
+    final password = controllerPassword.text.trim();
+
+    final user = ParseUser(username, password, null);
+
+    var response = await user.login();
+
+    if (response.success) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      showError(response.error!.message);
+    }
+  }
+}
---
+++ lib/back4app/sign_up.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final controllerUsername = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final controllerEmail = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            const Center(
+              child: Text('Create an account', style: TextStyle(fontSize: 16)),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              controller: controllerUsername,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  labelText: 'Username'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextField(
+              controller: controllerEmail,
+              keyboardType: TextInputType.emailAddress,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  labelText: 'E-mail'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextField(
+              controller: controllerPassword,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  labelText: 'Password'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+              height: 50,
+              child: TextButton(
+                child: const Text('SIGN UP'),
+                onPressed: () => doUserRegistration(),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void showSuccess() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          title: Text("Success!"),
+          content: Text("User was successfully created!"),
+          actions: <Widget>[
+            Text("OK"),
+          ],
+        );
+      },
+    );
+  }
+
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error!"),
+          content: Text(errorMessage),
+          actions: const <Widget>[
+            Text("OK"),
+          ],
+        );
+      },
+    );
+  }
+
+  void doUserRegistration() async {
+    final username = controllerUsername.text.trim();
+    final email = controllerEmail.text.trim();
+    final password = controllerPassword.text.trim();
+
+    final user = ParseUser.createUser(username, password, email);
+
+    var response = await user.signUp();
+
+    if (response.success) {
+      showSuccess();
+      Navigator.pushReplacementNamed(context, '/');
+    } else {
+      showError(response.error!.message);
+    }
+  }
+}
