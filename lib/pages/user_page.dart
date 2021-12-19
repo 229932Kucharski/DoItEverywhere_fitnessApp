33752f7da5fc1502c1e66e33cf110b19f70b3c99
Bartosz Pietrzyba <bartosz.pietrzyba@eficode.com>
2021-12-19 16:51:07 +0100

    user_page avatar

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -6,14 +6,33 @@ class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/background/bg_03.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: null /* add child content here */,
-    ));
+        body: SizedBox.expand(
+            child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/background/bg_03.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircleAvatar(
+                      radius: 90,
+                      backgroundColor: Color(0xFFFF9505),
+                      child: CircleAvatar(
+                        radius: 85,
+                        backgroundImage: AssetImage('assets/users/avatar.png'),
+                      ),
+                    ),
+                    Text(
+                      'Your name',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'SourceCodePro',
+                      ),
+                    ),
+                  ],
+                ))));
   }
 }
