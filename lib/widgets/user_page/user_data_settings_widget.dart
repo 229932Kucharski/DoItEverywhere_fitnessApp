45cf5d85a09d99655de13de5f5c71b62b7f2edc4
Bartosz Pietrzyba <bartosz.pietrzyba@eficode.com>
2022-01-12 22:46:25 +0100

    Setting height/weight/age

--- lib/widgets/user_page/user_data_settings_widget.dart
+++ lib/widgets/user_page/user_data_settings_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 class UserDataSettingsWidget extends StatefulWidget {
   const UserDataSettingsWidget({Key? key}) : super(key: key);
@@ -8,7 +9,59 @@ class UserDataSettingsWidget extends StatefulWidget {
 }
 
 class _UserDataSettingsWidgetState extends State<UserDataSettingsWidget> {
-  final controllerUsername = TextEditingController();
+  final controllerHeight = TextEditingController();
+  final controllerWeight = TextEditingController();
+  final controllerAge = TextEditingController();
+
+  ParseUser? currentUser;
+  int? height = 180;
+  int? weight = 70;
+  int? age = 20;
+
+  Future<ParseUser?> getUser() async {
+    currentUser = await ParseUser.currentUser() as ParseUser?;
+    return currentUser;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> updateUser() async {
+    await getUser();
+    String? id;
+    int? currentHeight;
+    int? currentWeight;
+    int? currentAge;
+
+    if (controllerHeight.text != '') height = int.parse(controllerHeight.text);
+    if (controllerWeight.text != '') weight = int.parse(controllerWeight.text);
+    if (controllerAge.text != '') age = int.parse(controllerAge.text);
+
+    QueryBuilder<ParseObject> queryUserData =
+        QueryBuilder<ParseObject>(ParseObject('UserData'))
+          ..whereEqualTo('user', currentUser);
+    final ParseResponse apiResponse = await queryUserData.query();
+    if (apiResponse.success && apiResponse.results != null) {
+      List<ParseObject> objects = apiResponse.results as List<ParseObject>;
+      for (ParseObject object in objects) {
+        id = object.objectId;
+        currentHeight = object.get('height');
+        currentWeight = object.get('weight');
+        currentAge = object.get('age');
+      }
+      currentHeight = height;
+      currentWeight = weight;
+      currentAge = age;
+      var user = ParseObject('UserData')
+        ..objectId = id
+        ..set('height', currentHeight)
+        ..set('weight', currentWeight)
+        ..set('age', currentAge);
+      await user.save();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +76,8 @@ class _UserDataSettingsWidgetState extends State<UserDataSettingsWidget> {
                 height: 16,
               ),
               TextField(
-                controller: controllerUsername,
-                keyboardType: TextInputType.text,
+                controller: controllerHeight,
+                keyboardType: TextInputType.number,
                 textCapitalization: TextCapitalization.none,
                 autocorrect: false,
                 decoration: const InputDecoration(
@@ -36,8 +89,8 @@ class _UserDataSettingsWidgetState extends State<UserDataSettingsWidget> {
                 height: 8,
               ),
               TextField(
-                controller: controllerUsername,
-                keyboardType: TextInputType.text,
+                controller: controllerWeight,
+                keyboardType: TextInputType.number,
                 textCapitalization: TextCapitalization.none,
                 autocorrect: false,
                 decoration: const InputDecoration(
@@ -49,8 +102,8 @@ class _UserDataSettingsWidgetState extends State<UserDataSettingsWidget> {
                 height: 8,
               ),
               TextField(
-                controller: controllerUsername,
-                keyboardType: TextInputType.text,
+                controller: controllerAge,
+                keyboardType: TextInputType.number,
                 textCapitalization: TextCapitalization.none,
                 autocorrect: false,
                 decoration: const InputDecoration(
@@ -69,7 +122,9 @@ class _UserDataSettingsWidgetState extends State<UserDataSettingsWidget> {
                       color: Color(0xFFFF9505),
                     ),
                   ),
-                  onPressed: () {},
+                  onPressed: () async {
+                    await updateUser();
+                  },
                 ),
               ),
             ],
